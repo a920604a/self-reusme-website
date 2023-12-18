3d8a1e00f511f1f8a7b6fd40880cfa7dbd32a435
a920604a <a920604a@gmail.com>
2023-12-18 00:08:35 +0000

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto: a920604a@gamil.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com/a920604a",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com/in/chen-yuan-2b4b7212b/",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com/@a920604a",
+  },
+  {
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com/users/22876310/a920604a",
+  },
+];
+
+const Header = () => {
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
+  const listItem = socials.map((social) => (
+    <a key={social.url} href={social.url}>
+      <FontAwesomeIcon icon={social.icon} size="2x" />
+    </a>
+  ));
+
+  // Handle header show/hide animation depending on the scroll direction
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    // Handle scroll events
+    const handleScroll = () => {
+      const currScrollPos = window.scrollY;
+      const currHeaderElement = headerRef.current;
+
+      if (!currHeaderElement) {
+        return;
+      }
+
+
+      if (prevScrollPos > currScrollPos) {
+        currHeaderElement.style.transform = "translateY(0)";
+      } else {
+        currHeaderElement.style.transform = "translateY(-200px)";
+      }
+
+      prevScrollPos = currScrollPos;
+    };
+
+    // Set up listeners for the scroll event
+    window.addEventListener("scroll", handleScroll);
+
+    // Remove listeners for the scroll event
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+      ref={headerRef}
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            <HStack spacing={8}>
+              {/* Add social media links based on the `socials` data */}
+              {listItem}
+            </HStack>
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              {/* Add links to Projects and Contact me section */}
+              <a href="/#porjects" className="nav-item" onClick={handleClick('projects')}>Projects</a>
+              <a href="/#contact-me" className="nav-item" onClick={handleClick('contactme')}>Contact Me</a>
+
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+
+      <header>
+
+      </header>
+
+    </Box>
+  );
+};
+export default Header;
---
+++ src/components/LandingSection.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Yu-An, Chen!";
+const bio1 = "A software Engineer";
+const bio2 = "specialised in Data/System Integration";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={10}>
+      <VStack>
+        <Avatar name="Pete" src="https://i.pravatar.cc/150?img=7" size="xl" />
+        <Heading size="xs">{greeting}</Heading>
+      </VStack>
+      <VStack>
+        <Heading size="3xl">{bio1}</Heading>
+        {/* <Heading size="3xl">{bio2}</Heading> */}
+
+      </VStack>
+
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
